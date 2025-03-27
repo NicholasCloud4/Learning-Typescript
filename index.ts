76a12baa5891f1ee241810c4fc03e7a16068ba43
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-03-27 12:07:32 -0400

    Added Pizza type

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+/**
+ * Challenge: Create a Pizza object type. It should include a `name`
+ * and a `price` property.
+ */
+
+type Pizza = {
+    name: string;
+    price: number;
+};
+
 const menu = [
     { name: "Margherita", price: 8 },
     { name: "Pepperoni", price: 10 },
@@ -9,11 +19,17 @@ let cashInRegister = 100;
 let nextOrderId = 1;
 const orderQueue = [];
 
-function addNewPizza(pizzaObj) {
+/**
+ * Challenge: teach TS that the pizzaObj is supposed to be a Pizza type.
+ * Then like before, look through the code to see if there are any new
+ * TS warnings to deal with (😉), and fix those issues
+ */
+
+function addNewPizza(pizzaObj: Pizza) {
     menu.push(pizzaObj);
 }
 
-function placeOrder(pizzaName) {
+function placeOrder(pizzaName: string) {
     const selectedPizza = menu.find((pizzaObj) => pizzaObj.name === pizzaName);
     if (!selectedPizza) {
         console.error(`${pizzaName} does not exist in the menu`);
@@ -35,9 +51,9 @@ function completeOrder(orderId: number) {
     return order;
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", cost: 12 });
-addNewPizza({ name: "BBQ Chicken", cost: 12 });
-addNewPizza({ name: "Spicy Sausage", cost: 11 });
+addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ name: "BBQ Chicken", price: 12 });
+addNewPizza({ name: "Spicy Sausage", price: 11 });
 
 placeOrder("Chicken Bacon Ranch");
 completeOrder(1);
