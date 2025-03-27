f614eb1c10fe923adf4bfde3c2fcef363a990f1d
Nicholas <91276870+NicholasCloud4@users.noreply.github.com>
2025-03-27 13:13:05 -0400

    Added Ids to Pizza

--- index.ts
+++ index.ts
@@ -1,12 +1,8 @@
 type Pizza = {
+    id: number;
     name: string;
     price: number;
 };
-
-/**
- * Challenge: using literal types and unions, update the Order status so that
- * it can only ever be "ordered" or "completed"
- */
 
 type Order = {
     id: number;
@@ -14,11 +10,11 @@ type Order = {
     status: "ordered" | "completed";
 };
 
-const menu = [
-    { name: "Margherita", price: 8 },
-    { name: "Pepperoni", price: 10 },
-    { name: "Hawaiian", price: 10 },
-    { name: "Veggie", price: 9 },
+const menu: Pizza[] = [
+    { id: 1, name: "Margherita", price: 8 },
+    { id: 2, name: "Pepperoni", price: 10 },
+    { id: 3, name: "Hawaiian", price: 10 },
+    { id: 4, name: "Veggie", price: 9 },
 ];
 
 let cashInRegister = 100;
@@ -55,9 +51,9 @@ function completeOrder(orderId: number) {
     return order;
 }
 
-addNewPizza({ name: "Chicken Bacon Ranch", price: 12 });
-addNewPizza({ name: "BBQ Chicken", price: 12 });
-addNewPizza({ name: "Spicy Sausage", price: 11 });
+addNewPizza({ id: 5, name: "Chicken Bacon Ranch", price: 12 });
+addNewPizza({ id: 6, name: "BBQ Chicken", price: 12 });
+addNewPizza({ id: 7, name: "Spicy Sausage", price: 11 });
 
 placeOrder("Chicken Bacon Ranch");
 placeOrder("Pepperoni");
